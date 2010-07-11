9d48e1090e9b9cfe69f1ba975567352307c05548
Alex Rea <adr34@cam.ac.uk>
2010-07-11 09:20:44 +0100

    Removed comments from diff

--- Library/Formula/plowshare.rb
+++ Library/Formula/plowshare.rb
@@ -4,7 +4,6 @@ class Plowshare <Formula
   head 'http://plowshare.googlecode.com/svn/trunk/', :using => :svn
   homepage 'http://code.google.com/p/plowshare'
 
-
   depends_on 'recode'
   depends_on 'imagemagick'
   depends_on 'tesseract'
@@ -27,14 +26,12 @@ end
 __END__
 --- a/src/lib.sh
 +++ b/src/lib.sh
-@@ -1,4 +1,10 @@
+@@ -1,4 +1,8 @@
  #!/bin/bash
 +shopt -s expand_aliases
 +alias sed='gsed'
-+#gsed Needed for almost everything
 +alias getopt='`brew --prefix gnu-getopt`/bin/getopt'
 +alias head='ghead'
-+#ghead Needed for mediafire at least. Readlink not needed
  #
  # This file is part of Plowshare.
  #
